df4c391c5e801abb57a12ebf34a583314f437ffd
Eric Zhang <ericzhang128@gmail.com>
2018-10-29 13:14:39 -0400

    setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const {} = require("./config.example.json");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login("token");
